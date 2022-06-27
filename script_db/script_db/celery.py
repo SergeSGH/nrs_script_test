455b73bce3c52150860256565a298242bafaefd4
Sergey <ssprihodko@yandex.ru>
2022-06-27 15:14:47 +0300

    docker tuning

--- script_db/script_db/celery.py
+++ script_db/script_db/celery.py
@@ -9,7 +9,7 @@ from celery.schedules import crontab
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'script_db.settings')
 
-app = Celery('script_db',)
+app = Celery('script_db', broker='amqp://rabbitmq:rabbitmq@rabbit:5672')
 
 app.conf.imports = ('data_collect.tasks',)
 
@@ -17,6 +17,10 @@ app.conf.beat_schedule = {
     'update database': {
         'task': 'data_collect.tasks.collect_data',
         'schedule': crontab(minute='*/1')
+    },
+    'update deadlines': {
+        'task': 'data_collect.tasks.check_deadlines',
+        'schedule': crontab(hour=8, minute=0, day_of_week=1)
     },
 }
 
